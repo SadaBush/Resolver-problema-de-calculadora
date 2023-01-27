4b0b237eba1eaf3d86438d99efaa33189b59fced
SadaBush <123669292+SadaBush@users.noreply.github.com>
2023-01-27 08:16:45 -0300

    Add files via upload

---
+++ Resolver problema de calculadora/Program.cs
@@ -0,0 +1,18 @@
+﻿// Resolver problema de Calculadora.
+
+//Calcular la suma de dos numeros.
+//Pedir y leer dos numeros ingresados por el usuario.
+//Moostrar el resultado en pantalla */
+
+int Numero1;
+int Numero2;
+int Resultado;
+
+Console.WriteLine("Ingrese el primer numero");
+Numero1= int.Parse(Console.ReadLine());
+
+Console.WriteLine("Ingrese el segundo numero");
+Numero2 = int.Parse(Console.ReadLine());
+
+Resultado = Numero1 + Numero2;
+Console.WriteLine(Resultado);
